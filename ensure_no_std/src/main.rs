a1d6713233d8245f511d51c801fcbc026aab0953
Warlock <internalmike@gmail.com>
2022-07-05 21:43:08 +0300

    ens

---
+++ ensure_no_std/src/main.rs
@@ -0,0 +1,18 @@
+#![feature(start)]
+
+#![deny(warnings)]
+
+#![no_std]
+
+#[panic_handler]
+extern fn panic(_info: &core::panic::PanicInfo) -> ! {
+    exit_no_std::exit(99)
+}
+
+use pc_ints::DOS_ERR_FILE_NOT_FOUND;
+
+#[start]
+pub fn main(_argc: isize, _argv: *const *const u8) -> isize {
+    assert_eq!(DOS_ERR_FILE_NOT_FOUND, 2);
+    0
+}

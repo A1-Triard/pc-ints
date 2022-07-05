f4ab453461f490adfcc78d89a27c35f693907502
Warlock <internalmike@gmail.com>
2022-07-06 00:37:42 +0300

    cfg(dos)

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,11 @@
 
 #![no_std]
 
+#[cfg(dos)]
 use core::arch::asm;
-use core::mem::{MaybeUninit, size_of};
+use core::mem::MaybeUninit;
+#[cfg(dos)]
+use core::mem::size_of;
 use core::num::NonZeroU8;
 
 pub const DOS_ERR_FUNC_NUM_INVALID: u8 = 1;
@@ -87,6 +90,14 @@ pub const DOS_ERR_NET_WRITE_FAULT: u8 = 88;
 pub const DOS_ERR_NET_FUNC_NOT_SUPPORTED: u8 = 89;
 pub const DOS_ERR_SYS_COMPONENT_NOT_INSTALLED: u8 = 90;
 
+#[cfg(not(dos))]
+#[allow(non_snake_case)]
+#[allow(unused_variables)]
+pub fn int_21h_ah_4Ch_exit(al_exit_code: u8) {
+    panic!("cfg(dos)");
+}
+
+#[cfg(dos)]
 #[allow(non_snake_case)]
 #[inline]
 pub fn int_21h_ah_4Ch_exit(al_exit_code: u8) {
@@ -104,6 +115,12 @@ pub struct DosVer {
     pub al_major: u8,
 }
 
+#[cfg(not(dos))]
+pub fn int_21h_ah_30h_dos_ver() -> DosVer {
+    panic!("cfg(dos)");
+}
+
+#[cfg(dos)]
 #[inline]
 pub fn int_21h_ah_30h_dos_ver() -> DosVer {
     let mut ax: u16;
@@ -130,9 +147,18 @@ pub struct AxErr {
     pub ax_err: u16,
 }
 
+#[cfg(dos)]
 const CF: u8 = 0x01;
+
+#[cfg(dos)]
 const ZF: u8 = 0x40;
 
+#[cfg(not(dos))]
+pub fn int_21h_ax_6601h_code_page() -> Result<CodePage, AxErr> {
+    panic!("cfg(dos)");
+}
+
+#[cfg(dos)]
 #[inline]
 pub fn int_21h_ax_6601h_code_page() -> Result<CodePage, AxErr> {
     let mut bx_active: u16;
@@ -158,6 +184,7 @@ pub fn int_21h_ax_6601h_code_page() -> Result<CodePage, AxErr> {
     }
 }
 
+#[cfg(dos)]
 #[inline]
 fn p32<T>(p: *const T) -> u32 {
     assert!(size_of::<*const T>() == size_of::<u32>());
@@ -169,6 +196,13 @@ pub struct AlLastCh {
     pub al_last_ch: u8,
 }
 
+#[cfg(not(dos))]
+#[allow(unused_variables)]
+pub fn int_21h_ah_02h_out_ch(dl_ch: u8) -> AlLastCh {
+    panic!("cfg(dos)");
+}
+
+#[cfg(dos)]
 #[inline]
 pub fn int_21h_ah_02h_out_ch(dl_ch: u8) -> AlLastCh {
     let mut ax: u16;
@@ -183,6 +217,13 @@ pub fn int_21h_ah_02h_out_ch(dl_ch: u8) -> AlLastCh {
     AlLastCh { al_last_ch: ax as u8 }
 }
 
+#[cfg(not(dos))]
+#[allow(unused_variables)]
+pub fn int_21h_ah_09h_out_str(dx_str_24h: *const u8) {
+    panic!("cfg(dos)");
+}
+
+#[cfg(dos)]
 #[inline]
 pub fn int_21h_ah_09h_out_str(dx_str_24h: *const u8) {
     unsafe {
@@ -200,6 +241,14 @@ pub struct AxHandle {
     pub ax_handle: u16,
 }
 
+#[cfg(not(dos))]
+#[allow(non_snake_case)]
+#[allow(unused_variables)]
+pub fn int_21h_ah_3Dh_open(dx_path_z: *const u8, al_mode: u8) -> Result<AxHandle, AxErr> {
+    panic!("cfg(dos)");
+}
+
+#[cfg(dos)]
 #[allow(non_snake_case)]
 #[inline]
 pub fn int_21h_ah_3Dh_open(dx_path_z: *const u8, al_mode: u8) -> Result<AxHandle, AxErr> {
@@ -228,6 +277,14 @@ pub struct AxRead {
     pub ax_read: u16,
 }
 
+#[cfg(not(dos))]
+#[allow(non_snake_case)]
+#[allow(unused_variables)]
+pub fn int_21h_ah_3Fh_read(bx_handle: u16, dx_cx_buf: &mut [MaybeUninit<u8>]) -> Result<AxRead, AxErr> {
+    panic!("cfg(dos)");
+}
+
+#[cfg(dos)]
 #[allow(non_snake_case)]
 #[inline]
 pub fn int_21h_ah_3Fh_read(bx_handle: u16, dx_cx_buf: &mut [MaybeUninit<u8>]) -> Result<AxRead, AxErr> {
@@ -258,6 +315,13 @@ pub struct AxWritten {
     pub ax_written: u16,
 }
 
+#[cfg(not(dos))]
+#[allow(unused_variables)]
+pub fn int_21h_ah_40h_write(bx_handle: u16, dx_cx_buf: &[u8]) -> Result<AxWritten, AxErr> {
+    panic!("cfg(dos)");
+}
+
+#[cfg(dos)]
 #[inline]
 pub fn int_21h_ah_40h_write(bx_handle: u16, dx_cx_buf: &[u8]) -> Result<AxWritten, AxErr> {
     let mut flags: u16;
@@ -293,6 +357,13 @@ pub struct AllocErr {
     pub bx_available_paragraphs: u16,
 }
 
+#[cfg(not(dos))]
+#[allow(unused_variables)]
+pub fn int_21h_ah_48h_alloc(bx_paragraphs: u16) -> Result<AxSegment, AllocErr> {
+    panic!("cfg(dos)");
+}
+
+#[cfg(dos)]
 #[inline]
 pub fn int_21h_ah_48h_alloc(bx_paragraphs: u16) -> Result<AxSegment, AllocErr> {
     let mut ebx_paragraphs = bx_paragraphs as u32;
@@ -321,6 +392,12 @@ pub struct BxSegment {
     pub bx_segment: u16,
 }
 
+#[cfg(not(dos))]
+pub fn int_21h_ah_62h_psp_addr() -> BxSegment {
+    panic!("cfg(dos)");
+}
+
+#[cfg(dos)]
 #[inline]
 pub fn int_21h_ah_62h_psp_addr() -> BxSegment {
     let mut bx_segment: u16;
@@ -339,6 +416,13 @@ pub struct AlChar {
     pub al_char: u8,
 }
 
+#[cfg(not(dos))]
+#[allow(non_snake_case)]
+pub fn int_21h_ah_06h_dl_FFh_inkey() -> Option<AlChar> {
+    panic!("cfg(dos)");
+}
+
+#[cfg(dos)]
 #[allow(non_snake_case)]
 #[inline]
 pub fn int_21h_ah_06h_dl_FFh_inkey() -> Option<AlChar> {
@@ -367,6 +451,13 @@ pub struct CxDxAddr {
     pub cx_dx_addr: u32,
 }
 
+#[cfg(not(dos))]
+#[allow(unused_variables)]
+pub fn int_31h_ax_0006h_segment_addr(bx_selector: u16) -> Result<CxDxAddr, AxErr> {
+    panic!("cfg(dos)");
+}
+
+#[cfg(dos)]
 #[inline]
 pub fn int_31h_ax_0006h_segment_addr(bx_selector: u16) -> Result<CxDxAddr, AxErr> {
     let mut flags: u16;
@@ -399,6 +490,13 @@ pub struct RmAlloc {
     pub dx_selector: u16,
 }
 
+#[cfg(not(dos))]
+#[allow(unused_variables)]
+pub fn int_31h_ax_0100h_rm_alloc(bx_paragraphs: u16) -> Result<RmAlloc, AllocErr> {
+    panic!("cfg(dos)");
+}
+
+#[cfg(dos)]
 #[inline]
 pub fn int_31h_ax_0100h_rm_alloc(mut bx_paragraphs: u16) -> Result<RmAlloc, AllocErr> {
     let mut flags: u16;
@@ -423,6 +521,13 @@ pub fn int_31h_ax_0100h_rm_alloc(mut bx_paragraphs: u16) -> Result<RmAlloc, Allo
     }
 }
 
+#[cfg(not(dos))]
+#[allow(unused_variables)]
+pub fn int_31h_ax_0101h_rm_free(dx_selector: u16) -> Result<(), AxErr> {
+    panic!("cfg(dos)");
+}
+
+#[cfg(dos)]
 #[inline]
 pub fn int_31h_ax_0101h_rm_free(dx_selector: u16) -> Result<(), AxErr> {
     let mut flags: u16;
@@ -450,6 +555,13 @@ pub struct AlErr {
     pub al_err: NonZeroU8,
 }
 
+#[cfg(not(dos))]
+#[allow(unused_variables)]
+pub fn int_10h_ah_00h_set_video_mode(al_mode: u8) -> Result<(), AlErr> {
+    panic!("cfg(dos)");
+}
+
+#[cfg(dos)]
 #[inline]
 pub fn int_10h_ah_00h_set_video_mode(al_mode: u8) -> Result<(), AlErr> {
     let mut al_err: u16;
@@ -470,6 +582,14 @@ pub struct VideoMode {
     pub bh_active_page: u8,
 }
 
+#[cfg(not(dos))]
+#[allow(non_snake_case)]
+#[allow(unused_variables)]
+pub fn int_10h_ah_0Fh_video_mode() -> VideoMode {
+    panic!("cfg(dos)");
+}
+
+#[cfg(dos)]
 #[allow(non_snake_case)]
 #[inline]
 pub fn int_10h_ah_0Fh_video_mode() -> VideoMode {

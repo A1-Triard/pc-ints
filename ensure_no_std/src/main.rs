0f45db323759e3b0e4173021055548752915c05b
Warlock <internalmike@gmail.com>
2022-07-06 16:18:33 +0300

    avoid circular dependencies

--- ensure_no_std/src/main.rs
+++ ensure_no_std/src/main.rs
@@ -8,9 +8,23 @@
 #[link(name="msvcrt")]
 extern { }
 
+#[cfg(windows)]
+fn exit(code: u8) -> ! {
+    unsafe { winapi::um::processthreadsapi::ExitProcess(
+        code as winapi::shared::minwindef::UINT
+    ); }
+    #[allow(clippy::empty_loop)]
+    loop { }
+}
+
+#[cfg(not(windows))]
+fn exit(code: u8) -> ! {
+    unsafe { libc::exit(code as u16 as i16 as libc::c_int) }
+}
+
 #[panic_handler]
 extern fn panic(_info: &core::panic::PanicInfo) -> ! {
-    exit_no_std::exit(99)
+    exit(99)
 }
 
 use pc_ints::DOS_ERR_FILE_NOT_FOUND;
